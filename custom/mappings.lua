483b338cae9f14b9fa03f29ee5c920cc335c0f99
Youtirsin <youtirsin@qq.com>
2023-04-30 12:04:46 +0800

    first

---
+++ custom/mappings.lua
@@ -0,0 +1,7 @@
+M.youtirsin = {
+  n = {
+    ["<leader>o"] = { "<cmd>SymbolsOutline<CR>", "SymbolsOutline"},
+    ["<leader>e"] = { "<cmd>HopWord<CR>", "Hop Word"},
+    ["<leader>j"] = { "<cmd>HopLineStart<CR>", "Hop Line Start"},
+  },
+}
